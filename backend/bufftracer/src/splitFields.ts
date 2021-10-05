8632426a1732d9864ee1da58ef6ba9ccfbf5e60e
philippemiguet <philippe.miguet@gmail.com>
2021-10-05 18:01:39 +0200

    add more fields and skip typename fields

--- backend/bufftracer/src/splitFields.ts
+++ backend/bufftracer/src/splitFields.ts
@@ -13,12 +13,27 @@ export function splitFields(fields: Array<string>): APIMetadataFields {
     'fields8',
     'fields9',
     'fields10',
+    'fields11',
+    'fields12',
+    'fields13',
+    'fields14',
+    'fields15',
+    'fields16',
+    'fields17',
+    'fields18',
+    'fields19',
+    'fields20',
   ]
 
   let currentKeyIndex = 0
   let currentTagValue: Array<string> = []
 
   for (const field of fields) {
+    // Skip invalid fields
+    if (!isValidField(field)) {
+      continue
+    }
+
     if (isWithinTagValueLimit(currentTagValue, field) === false) {
       // Let's assign this tag value to the current key and start a new one
       apiMetadatafields[keys[currentKeyIndex]] = currentTagValue
@@ -30,6 +45,10 @@ export function splitFields(fields: Array<string>): APIMetadataFields {
   // Let's asign the final tag value
   apiMetadatafields[keys[currentKeyIndex]] = currentTagValue
   return apiMetadatafields
+}
+
+function isValidField(field: string): boolean {
+  return field.indexOf('__typename') === -1
 }
 
 function isWithinTagValueLimit(
